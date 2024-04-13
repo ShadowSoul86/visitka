af3a3d7915c1fc78a7cf96f6b637514e957d719b
fteeliy <veselov-vk@wdwain.ru>
2024-04-13 16:07:36 +0300

    адаптив модалки

--- components/Modals/Modal.tsx
+++ components/Modals/Modal.tsx
@@ -6,14 +6,15 @@ const Modal: FC<any> = () => {
 
   return isOpen ? (
     <div className="modal-bg">
-      <div className="modal w-[468px] medium:w-[635px]">
+      <div className="modal w-[468px] medium:w-[635px] pad:w-[832px] mac:w-[900px] full:w-[1010px]">
         <button
-          className="absolute z-50 top-0 right-[-50px]"
+          className="absolute z-50 top-[15px] right-[-40px]"
           onClick={() => closeModal()}
         >
           <svg
-            width="34"
-            height="34"
+            className="medium:w-[30px] medium:h-[30px]"
+            width={`25`}
+            height={`25`}
             viewBox="0 0 34 34"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -33,11 +34,17 @@ const Modal: FC<any> = () => {
           </svg>
         </button>
 
-        <div className="flex flex-col gap-[18px] max-w-[407px] text-center mx-auto">
-          <h2 className={`font-sans font-bold text-[40px] text-center`}>
+        <div className="flex flex-col gap-[18px] text-center mx-auto max-w-[407px] medium:max-w-[532px] mac:max-w-[569px] full:max-w-[630px]">
+          <h2
+            className={`font-sans font-bold text-center text-[40px] medium:text-[48px]`}
+          >
             {modalData?.title}
           </h2>
-          <div className={`text-[16px] text-my-brown`}>{modalData?.descr}</div>
+          <div
+            className={`text-my-brown text-[16px] pad:text-[20px] full:text-[24px]`}
+          >
+            {modalData?.descr}
+          </div>
         </div>
 
         <ul className="mt-[40px] mb-[50px]">
@@ -46,10 +53,12 @@ const Modal: FC<any> = () => {
               <li className="flex flex-col gap-[15px]" key={i}>
                 <div className={`h-[1px] w-full bg-my-modal-line`} />
                 <div
-                  className={`text-[16px] flex justify-between items-center`}
+                  className={`text-[16px] pad:text-[18px] full:text-[20px] flex justify-between`}
                 >
-                  <div className="font-bold">{title}</div>
-                  <div className="w-full font-normal text-my-grey text-left max-w-[265px] ">
+                  <div className="font-bold w-full max-w-[93px] medium:max-w-[200px] full:max-w-[300px] text-wrap">
+                    {title}
+                  </div>
+                  <div className="w-full font-normal text-my-grey text-left max-w-[265px] medium:max-w-[360px] pad:max-w-[466px] mac:max-w-[496px] full:max-w-[580px]">
                     {text}
                   </div>
                 </div>
